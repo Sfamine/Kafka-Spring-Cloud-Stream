79563a82462e30ab839d9c5a527851c7ff84c241
Amine <amine.sfaira@gmail.com>
2024-01-28 22:23:16 +0100

    first commit

---
+++ src/main/java/org/sid/atelier6/entities/PageEvent.java
@@ -0,0 +1,18 @@
+package org.sid.atelier6.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PageEvent {
+    private String name; 
+    private String user;
+    private Date date;
+    private long duration;
+}
---
+++ src/test/java/org/sid/atelier6/Atelier6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.atelier6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atelier6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
